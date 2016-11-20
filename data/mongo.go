aa1891bb895ee75fea45681d1134bf56c527690e
Eleanor Deal <eleanor.deal@gmail.com>
2016-11-20 18:22:35 +0000

    Use env var for database name

--- data/mongo.go
+++ data/mongo.go
@@ -12,7 +12,8 @@ import (
 
 //Mongo holds the details of an established session with MongoDB
 type Mongo struct {
-	Session *mgo.Session
+	Session  *mgo.Session
+	Database string
 }
 
 var DB *Mongo
@@ -20,11 +21,12 @@ var DB *Mongo
 //StartSession initializes a connection to MongoDB for use throughout the application.
 func StartSession() {
 	mongoURI := os.Getenv("MONGODB_URI")
+	db := os.Getenv("DB_NAME")
 	s, err := mgo.Dial(mongoURI)
 	if err != nil {
 		panic(err)
 	}
-	DB = &Mongo{Session: s}
+	DB = &Mongo{Session: s, Database: db}
 }
 
 //FindTechnology uses the name of the technology to return the JSON document
@@ -32,7 +34,7 @@ func StartSession() {
 func (m *Mongo) FindTechnology(tech string) *Technology {
 	s := m.Session.Clone()
 	defer s.Close()
-	c := s.DB("collab").C("technology")
+	c := s.DB(m.Database).C("technology")
 
 	result := &Technology{}
 
@@ -46,7 +48,7 @@ func (m *Mongo) FindTechnology(tech string) *Technology {
 func (m *Mongo) UpdateTechnology(tech *Technology) *mgo.ChangeInfo {
 	s := m.Session.Clone()
 	defer s.Close()
-	c := s.DB("collab").C("technology")
+	c := s.DB(m.Database).C("technology")
 
 	info, err := c.UpsertId(tech.ID, tech)
 	if err != nil {
@@ -67,7 +69,7 @@ func (m *Mongo) NewTechnology(tech string, user string, trigger string) {
 
 	s := m.Session.Clone()
 	defer s.Close()
-	c := s.DB("collab").C("technology")
+	c := s.DB(m.Database).C("technology")
 	if err := c.Insert(new); err != nil {
 		log.Fatal(err)
 	}
